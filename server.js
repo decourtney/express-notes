10bfc2b06403320705feeb393cbeca5495b98388
decourtney <53956789+decourtney@users.noreply.github.com>
2023-01-25 17:13:12 -0600

    still trying

--- server.js
+++ server.js
@@ -61,5 +61,5 @@ app.get('*', (req, res) =>
     res.sendFile(path.join(__dirname, 'public', 'index.html')));
 
 app.listen(PORT, () =>
-    console.log(`App listening at http://localhost:${PORT} 🚀`)
+    console.log(`App listening on port ${PORT} 🚀`)
 );
