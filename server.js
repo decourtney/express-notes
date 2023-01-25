5bbff9877784a422a3ac89e59a9c456c07e970a6
decourtney <53956789+decourtney@users.noreply.github.com>
2023-01-25 17:00:10 -0600

    PORT settings

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 const { readFromFile, readAndAppend, readAndRemove } = require('./helpers/fsUtils');
 const uuid = require('./helpers/uuid');
